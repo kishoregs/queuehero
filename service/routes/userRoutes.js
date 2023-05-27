e538cac5ee62d5ea3acb022ffee5465e968bd812
Kishore Shiraguppi <kishor4u@gmail.com>
2023-05-27 01:11:45 -0400

    fixed darkmode appeareance

--- service/routes/userRoutes.js
+++ service/routes/userRoutes.js
@@ -59,7 +59,7 @@ router.post("/forgot-password", (req, res) => {
     const mailOptions = {
       from: process.env.EMAIL_USERNAME,
       to: user.email,
-      subject: "Link to reset password",
+      subject: "QueueHero  | Link to reset your password",
       text:
         "You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n" +
         "Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n" +
