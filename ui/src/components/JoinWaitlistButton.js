b34acfc2544e25197759859ce502c86de6abbe03
Kishore Shiraguppi <kishor4u@gmail.com>
2024-02-21 23:45:12 -0500

    revisitng

--- ui/src/components/JoinWaitlistButton.js
+++ ui/src/components/JoinWaitlistButton.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import { useContext, useState } from "react";
 import api from "../api";
 import { AuthContext } from "../context/AuthContext";
@@ -14,7 +16,6 @@ function JoinWaitlistButton({
 
   const handleJoinWaitlist = async () => {
     try {
-
       if (joined) {
         await api.delete(`/businesses/${businessId}/unjoin-waitlist`, {
           params: { customerId: user._id },
