85cfaa877b111637115d9883ae24cad1e1e5df02
Kishore Shiraguppi <kishor4u@gmail.com>
2024-02-22 22:26:10 -0500

    Updates

---
+++ precache-manifest.20d247abfa172265494c0ee0a76eccf6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf986a0039ae275823e71c0f30bd7b6",
+    "url": "/queuehero/index.html"
+  },
+  {
+    "revision": "04791ab29cbff22dbad1",
+    "url": "/queuehero/static/css/main.d0c73264.chunk.css"
+  },
+  {
+    "revision": "153f9423cdcc1b995a83",
+    "url": "/queuehero/static/js/2.c9347d5c.chunk.js"
+  },
+  {
+    "revision": "c4a92d83bff9d017a1c3",
+    "url": "/queuehero/static/js/3.d637a5b4.chunk.js"
+  },
+  {
+    "revision": "04791ab29cbff22dbad1",
+    "url": "/queuehero/static/js/main.24f39424.chunk.js"
+  },
+  {
+    "revision": "3b6b5e0c03b511b65f8e",
+    "url": "/queuehero/static/js/runtime~main.838c5522.js"
+  },
+  {
+    "revision": "6188410a315be6879dd0fa375dd96984",
+    "url": "/queuehero/static/media/QueueHero.6188410a.png"
+  }
+]);
